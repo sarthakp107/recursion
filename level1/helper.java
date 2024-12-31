a9c6d2ae08ceb0faf4c9807e8e7a056f551e5f90
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2024-12-31 11:19:18 +1100

    palinrome rec

--- level1/helper.java
+++ level1/helper.java
@@ -1,7 +1,7 @@
 public class helper {
     public static void main(String[] args) {
-      rev(1234);
-      System.out.println(sum);
+    //   rev(1234);
+      System.out.println(rev2(123));
     }
 
     static int sum = 0;
@@ -17,12 +17,16 @@ public class helper {
 
     static int rev2(int n){
         int digits = (int)(Math.log10(n)) + 1;
-        // return helper(n, digits);
+        return helper(n, digits);
     }
     
-    // private static int helper(int n , int digits){
-
-    // }
+    private static int helper(int n , int digits){
+        if(n%10 == n){
+            return n;
+        }
+        int rem = n%10;
+        return rem * (int)(Math.pow(10, digits-1)) + helper(n/10, digits-1);
+    }
 
 
 
---
+++ level1/palindrome.java
@@ -0,0 +1,24 @@
+public class palindrome {
+    public static void main(String[] args) {
+    //    rev(1234); 
+       System.out.println(palin(121));
+    }    
+
+    static int sum = 0;
+    public static int rev(int n){
+        if(n == 0){
+            return 0;
+        }
+        int rem = n%10;
+        sum = sum*10 + rem;
+        rev(n/10);
+        return sum;
+    }
+
+    public static boolean palin(int n){
+        if(n == rev(n)){
+            return true;
+        }
+        return false;
+    }
+}
