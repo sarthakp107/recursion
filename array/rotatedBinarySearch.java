b229e4dcab9f554f22e91c7893e0ef7eea365aa8
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-05 16:48:29 +1100

    logic

---
+++ array/rotatedBinarySearch.java
@@ -0,0 +1,20 @@
+public class rotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3};
+    }
+/*  s     m        e
+ * [5,6,7,8,9,1,2,3]
+ *  0,1,2,3,4,5,6,7
+ * 
+ * target = 7;
+ * 
+ * 1) m ==target
+ * 2) s < m : numbers till s-m are sorted
+ *  2.1) check till m-1;
+ * 
+ */
+
+    public static void binarySearchForRotated(int[] arr, int target){
+          
+    }
+}
