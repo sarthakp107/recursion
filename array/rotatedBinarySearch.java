044c25beecc8334d17d7ecaa71c4652d2c33a8bb
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-06 13:11:19 +1100

    dunno

--- array/rotatedBinarySearch.java
+++ array/rotatedBinarySearch.java
@@ -14,7 +14,21 @@ public class rotatedBinarySearch {
  * 
  */
 
-    public static void binarySearchForRotated(int[] arr, int target){
-          
+    public static int binarySearchForRotated(int[] arr, int target , int start , int end){
+        while(start<=end){
+            int mid = start+ (end - start)/2;
+
+            //first case: 
+            if(target == arr[mid]) {
+                return mid;
+            }
+
+            //case 2:
+            if(arr[start] < arr[mid]){
+                //it is sorted
+                binarySearchForRotated(arr, target, start, mid-1);
+            }
+            
+        }
     }
 }
