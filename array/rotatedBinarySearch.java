a264bc8397769048b4214b8197389f508e5e7933
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-06 13:55:37 +1100

    recursive binarySearch for rotated array

--- array/rotatedBinarySearch.java
+++ array/rotatedBinarySearch.java
@@ -1,34 +1,34 @@
 public class rotatedBinarySearch {
     public static void main(String[] args) {
         int[] arr = {5,6,7,8,9,1,2,3};
+        System.out.println(binarySearchForRotated(arr, 7, 0, arr.length - 1));
     }
-/*  s     m        e
- * [5,6,7,8,9,1,2,3]
- *  0,1,2,3,4,5,6,7
- * 
- * target = 7;
- * 
- * 1) m ==target
- * 2) s < m : numbers till s-m are sorted
- *  2.1) check till m-1;
- * 
- */
+
 
     public static int binarySearchForRotated(int[] arr, int target , int start , int end){
-        while(start<=end){
-            int mid = start+ (end - start)/2;
+    if(start > end){
+        return -1;
+    }
+    int mid = start + (end - start)/2;
 
-            //first case: 
-            if(target == arr[mid]) {
-                return mid;
-            }
+    if( arr[mid] == target){
+        return mid;
+    }
 
-            //case 2:
-            if(arr[start] < arr[mid]){
-                //it is sorted
-                binarySearchForRotated(arr, target, start, mid-1);
-            }
-            
+    if(arr[start] <= arr[mid]){
+        if(target >= arr[start] && target <= arr[mid]){
+            //end = mid -1;
+            return binarySearchForRotated(arr, target, start, mid-1);
+        }else{
+            binarySearchForRotated(arr, target, mid + 1, end);
         }
+    }
+
+    if(target >= arr[mid] && target <= arr[end]){
+        return binarySearchForRotated(arr, target, mid + 1, end); 
+    }else{
+        return binarySearchForRotated(arr, target, mid -1, end);
+    }
+        
     }
 }
