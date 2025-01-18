f331c42b62b5d67bd106e5dae30505ab5a6e6018
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-18 16:26:27 +1100

    ouno

---
+++ mergesort/mergesort.java
@@ -0,0 +1,19 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+
+public class mergesort {
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,4,3};
+    }
+    static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+        
+        int mid = arr.length /2;
+        int[] left =  Arrays.copyOfRange()
+    }
+}
