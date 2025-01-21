2a0cf6613b898e796da95969f31a6a387dc1671d
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-21 13:41:46 +1100

    mergesort

--- mergesort/mergesort.java
+++ mergesort/mergesort.java
@@ -1,5 +1,4 @@
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 
@@ -7,6 +6,8 @@ import java.util.Arrays;
 public class mergesort {
     public static void main(String[] args) {
         int[] arr = {1,2,5,4,3};
+       arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static int[] mergeSort(int[] arr){
         if(arr.length == 1){
@@ -14,7 +15,40 @@ public class mergesort {
         }
         
         int mid = arr.length /2;
-        int[] left =  Arrays.copyOfRange(int[] arr, 0 mid);
-        int[] left =  Arrays.copyOfRange();
+
+        int[] left =  mergeSort(Arrays.copyOfRange( arr, 0, mid));
+        int[] right =  mergeSort( Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge( left,  right);
+    }
+
+    static int[] merge(int[] first, int[] second){
+        int[] mix = new int [first.length + second.length];
+        int i =0;
+        int j =0;
+        int k =0;
+
+        while(i< first.length && j< second.length){
+            if(first[i] < second[j]){
+                mix[k] = first[i];
+                i++;
+            }else{
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < first.length){
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while(j < second.length){
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
     }
 }
