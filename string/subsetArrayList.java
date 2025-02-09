65f717bbde9e71244dca2db14c40dc8ef1666e62
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-02-09 17:33:55 +1100

    another method array;list subset

--- string/subsetArrayList.java
+++ string/subsetArrayList.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class subsetArrayList {
     public static void main(String[] args) {
-        ArrayList<String> result = subset("", "abc");
+        ArrayList<String> result = subset1("", "abc");
+        
         System.out.println(result);
     }
 
@@ -24,5 +25,20 @@ public class subsetArrayList {
         result.addAll(exclude);
 
         return result;
+    } 
+    public static ArrayList<String> subset1(String p, String up){
+        
+        if(up.isEmpty()){
+            ArrayList<String> result = new ArrayList<>();
+            result.add(p);
+            return result;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> include = subset1(p + ch, up.substring(1));
+        ArrayList<String> exclude = subset1(p, up.substring(1));
+        include.addAll(exclude);
+        return include;
     } 
 }
