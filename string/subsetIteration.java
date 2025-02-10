0ec252ebb745961822ca90e0a8336b553a07fa42
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-02-10 16:47:13 +1100

    permutation rec

---
+++ string/subsetIteration.java
@@ -0,0 +1,24 @@
+
+
+public class subsetIteration {
+    public static void main(String[] args) {
+        
+    } 
+
+    public static void permutation(String p , String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        for(int i =0 ; i<= p.length(); i++){
+            String f = p.substring(0,i);
+            String l = p.substring(i,p.length());
+            String ans = f + ch + l ;
+            permutation(ans, up.substring(1));
+        }
+    }
+
+
+}
