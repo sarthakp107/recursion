2d346c4916335b9de6fcdd6e07821f1d2a916f44
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-02-12 17:59:24 +1100

    maze paths Array list

--- backtracking/maze.java
+++ backtracking/maze.java
@@ -1,9 +1,13 @@
 
+import java.util.ArrayList;
+
+
 
 public class maze {
     public static void main(String[] args) {
         // System.out.println(pathCount(3,3));
-        path("" , 3,3);
+        // path("" , 3,3);
+        System.out.println(pathRet("", 3, 3));
     }
 
     public static int pathCount(int row, int column){
@@ -30,5 +34,25 @@ public class maze {
         if(column>1){
             path(p + "R" , row, column -1);
         }
+    }
+
+
+
+    public static ArrayList<String> pathRet( String p,int row , int column){
+        if(row == 1 && column == 1){
+            ArrayList<String> ans= new ArrayList<>();
+            ans.add(p);
+            return ans;
+        }
+
+        ArrayList<String> list  = new ArrayList<>();
+
+        if(row>1){
+          list.addAll(pathRet(p + "D" , row-1, column));
+        }
+        if(column>1){
+            list.addAll(pathRet(p + "R" , row, column -1));
+        }
+        return list;
     }
 }
